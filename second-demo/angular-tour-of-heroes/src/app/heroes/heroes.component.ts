0da228d467e57f13e28884df6d0d165b9f576e92
chaidaxuan <1032127641@qq.com>
2019-07-16 13:22:05 +0800

    英雄列表

--- second-demo/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ second-demo/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,16 +1,26 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero'
+import { Hero } from '../hero';
 import { from } from 'rxjs';
+import { HEROES } from '../mock-heroes';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.less']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'windstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'windstorm'
+  };
+  heroes = HEROES;
+  selectedHero: Hero;
   constructor() { }
 
   ngOnInit() {
+
+  }
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
   }
 
 }
